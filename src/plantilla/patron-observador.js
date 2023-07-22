84b265afe474ede65ba5554b2c44b1ad3fc4f896
nol4lej <nome.ahumada@outlook.com>
2023-07-22 19:37:35 -0400

    Plantilla PatronObservador creada

---
+++ src/plantilla/patron-observador.js
@@ -0,0 +1,21 @@
+class PlantillaPatronObservador{
+
+    constructor(){
+        this.observadores = []
+    }
+
+    suscribir(observador){
+        this.observadores.push(observador)
+    }
+
+    desuscribir(suscriptor){
+        this.observadores = this.observadores.filter(observador => observador !== suscriptor)
+    }
+
+    notificar(evento){
+        this.observadores.forEach(observador => {
+            observador(evento)
+        })
+    }
+    
+}
