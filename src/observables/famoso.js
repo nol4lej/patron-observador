68c6bc1848b92da9fde5d4b46d212c473a58aea1
nol4lej <nome.ahumada@outlook.com>
2023-07-22 20:01:05 -0400

    Observable completo

--- src/observables/famoso.js
+++ src/observables/famoso.js
@@ -5,6 +5,7 @@ class Famoso extends PlantillaPatronObservador{
     constructor(){
         super()
         this.texto = ""
+        this.recibirTexto()
     }
 
     notificar(nuevoTexto){
@@ -12,9 +13,13 @@ class Famoso extends PlantillaPatronObservador{
         super.notificar(this.texto)
     }
 
+    recibirTexto(){
+        const textarea = document.getElementById("textarea-observable")
+        textarea.addEventListener("input", (evento) =>{
+            famosoObservable.notificar(evento.target.value)
+        })
+    }
+
 }
 
 const famosoObservable = new Famoso()
-
-famosoObservable.notificar("esto es una notificacion")
-console.log(famosoObservable)
